f0e5e82bb0a057f994f51d10249ef4739d0b0ba5
chandni-rani <ranichandni055@gmail.com>
2025-12-06 18:05:58 +0530

    Done

---
+++ part4/task9.js
@@ -0,0 +1,8 @@
+/* You have an array teaMenu containing "Green", "Black", "Oolong", "White", "Herbal".
+Find the legth of the array and store it in a variable named `menuLength`. */
+
+let teaMenu = ["Green-tea", "Black-tea", "Oolong-tea", "White-tea", "Herbal-tea"];
+
+let menuLength = teaMenu.length;
+
+console.log(menuLength);
